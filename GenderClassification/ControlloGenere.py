87e544995787c83df553a6f129bbf22e993dcf88
DanieleCesarano96 <45485079+DanieleCesarano96@users.noreply.github.com>
2020-05-20 15:34:57 +0200

    .

--- GenderClassification/ControlloGenere.py
+++ GenderClassification/ControlloGenere.py
@@ -1,7 +1,10 @@
 import numpy as np
+import pandas as pd
 
 countM=0
 countF=0
+maggiore=0
+count=0
 
 array1 = np.genfromtxt ("file1.csv", delimiter = ",", skip_header = 1)
 array1 = array1.astype(int)
@@ -15,7 +18,44 @@ for genere in array1:
     elif genere[66]==1:
         countF = countF + 1
 
-print(str(countF)+","+str(countM))
+if countM > countF:
+    maggiore=countF
+else:
+    maggiore=countM
 
-for i in range(0):
-    if array1[i][66]==0:
+array1finale = [[0 for y in range(66)] for x in range(maggiore*2)]
+array2finale = [[0 for y in range(66)] for x in range(maggiore*2)]
+
+countM=0
+countF=0
+
+for i in array1:
+    if i[66] == 0:
+        if countM < maggiore:
+            array1finale[count]= i
+            countM=countM+1
+            count = count + 1
+    elif i[66] == 1:
+        if countF < maggiore:
+            array1finale[count]= i
+            countF=countF + 1
+            count=count+1
+
+countM=0
+countF=0
+count=0
+
+for i in array2:
+    if i[66] == 0:
+        if countM < maggiore:
+            array2finale[count]= i
+            countM=countM+1
+            count = count + 1
+    elif i[66] == 1:
+        if countF < maggiore:
+            array2finale[count]= i
+            countF=countF + 1
+            count = count + 1
+
+pd.DataFrame(array1finale).to_csv("file1finale.csv")
+pd.DataFrame(array2finale).to_csv("file2finale.csv")
