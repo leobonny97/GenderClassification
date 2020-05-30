86b5433dc4b7deaaaff3b466e0496bc2df10e6ea
DanieleCesarano96 <45485079+DanieleCesarano96@users.noreply.github.com>
2020-05-30 09:20:07 +0200

    .

--- GenderClassification/ControlloGenere6.py
+++ GenderClassification/ControlloGenere6.py
@@ -10,9 +10,9 @@ array6 = np.genfromtxt ("file6.csv", delimiter = ",", skip_header = 1)
 array6 = array6.astype(int)
 
 for genere in array6:
-    if genere[66]==0:
+    if genere[82]==0:
         countM = countM + 1
-    elif genere[66]==1:
+    elif genere[82]==1:
         countF = countF + 1
 
 if countM > countF:
@@ -20,18 +20,18 @@ if countM > countF:
 else:
     maggiore=countM
 
-array6finale = [[0 for y in range(66)] for x in range(maggiore*2)]
+array6finale = [[0 for y in range(82)] for x in range(maggiore*2)]
 
 countM=0
 countF=0
 
 for i in array6:
-    if i[66] == 0:
+    if i[82] == 0:
         if countM < maggiore:
             array6finale[count]= i
             countM=countM+1
             count = count + 1
-    elif i[66] == 1:
+    elif i[82] == 1:
         if countF < maggiore:
             array6finale[count]= i
             countF=countF + 1
