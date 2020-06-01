6d09e1804d44eef7669e8b4c19b3673ef3c66341
DanieleCesarano96 <45485079+DanieleCesarano96@users.noreply.github.com>
2020-06-01 20:41:21 +0200

    .

---
+++ GenderClassification/AggiuntaPosaColoreDimensioneViso6.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+
+array1 = np.genfromtxt ("file6finaleConEtà.csv", delimiter = ",", skip_header = 1)
+array1 = array1.astype(int)
+
+array = np.genfromtxt ("train_gtruth.csv", delimiter = ",", skip_header = 1)
+array= array.astype(int)
+
+file = [[0 for y in range(86)] for x in range(array1.shape[0])]
+
+
+count=0
+
+for (a) in array1:
+    for (b) in array:
+        if (a[81]) == (b[0]):
+            file[count] = a[1:]
+            file[count] = np.append(file[count], [b[3]])
+            file[count] = np.append(file[count], [b[5]])
+            file[count] = np.append(file[count], [b[8]])
+            count = count + 1
+            print(count)
+            break
+
+pd.DataFrame(file).to_csv("file6finaleConEtàPosaColoreDimensioneViso.csv")
+
