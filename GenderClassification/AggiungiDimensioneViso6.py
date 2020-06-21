fc78e95d260f2c039ce9adbf9929a75f9406e790
leobonny97 <45544149+leobonny97@users.noreply.github.com>
2020-06-21 14:47:30 +0200

    Risultati classificazione con landmark e dimensione volto

---
+++ GenderClassification/AggiungiDimensioneViso6.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+
+array1 = np.genfromtxt ("file6finale.csv", delimiter = ",", skip_header = 1)
+array1 = array1.astype(int)
+
+array = np.genfromtxt ("train_gtruth.csv", delimiter = ",", skip_header = 1)
+array= array.astype(int)
+
+
+file = [[0 for y in range(83)] for x in range(array1.shape[0])]
+
+
+count=0
+
+for (a) in array1:
+    for (b) in array:
+        if (a[82]) == (b[0]):
+            file[count] = a[2:]
+            file[count] = np.append(file[count], [b[8]])
+            count = count + 1
+            print(count)
+            break
+
+pd.DataFrame(file).to_csv("file6finaleConDimensioneVolto.csv")
